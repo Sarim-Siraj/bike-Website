4337b05b50b8689be230750d1338a01b985ef77b
Sarim-Siraj <sarimsirajss@gmail.com>
2024-12-01 14:38:39 +0500

    update

---
+++ app.js
@@ -0,0 +1,252 @@
+var bikes = {
+  SUZUKI: {
+    SuzukiGS150 : {
+      name: "Suzuki GS 150 ",
+      price: "PKR 389,000",
+      category: "Bike",
+      caption: "Suzuki GS 150 is a popular 150cc bike in the Pakistani market. Suzuki GS 150 is said to be an accomplished bike for several reasons. One of the most prominent features this bike holds is the electric start system along with as well. An extremely energetic performance of 150cc 4 cooled SOHC engine is admired by the youth to a considerable extent." , 
+      color: ["Black", "Red"],
+      img: "https://cache1.pakwheels.com/system/bike_model_pictures/1403/original/1.jpg?1666261928",
+    },
+    SuzukiGD110S2024 : {
+      name: "Suzuki GD 110S  ",
+      price: "PKR 359,000",
+      category: "Bike",
+      caption: "Suzuki GS 150 is a popular  bike in the Pakistani market. Suzuki is said to be an accomplished bike for several reasons. One of the most prominent features this bike holds is the electric start system along with as well. An extremely energetic performance of 4 cooled SOHC engine is admired by the youth to a considerable extent." , 
+      color: ["Black", "Red"],
+      img: "https://cache4.pakwheels.com/system/bike_model_pictures/1394/original/1.jpg?1666006806",
+    },
+    SuzukiGSX1252024 : {
+      name: "Suzuki GSX 125 2024  ",
+      price: "PKR 499,000",
+      category: "Bike",
+      caption: "Suzuki GS 150 is a popular bike in the Pakistani market. Suzuki is said to be an accomplished bike for several reasons. One of the most prominent features this bike holds is the electric start system along with as well. An extremely energetic performance of 4 cooled SOHC engine is admired by the youth to a considerable extent." , 
+      color: ["Black", "Red"],
+      img: "https://cache3.pakwheels.com/system/bike_model_pictures/1438/original/1.jpg?1667199564",
+    },
+    SuzukiGR150 : {
+      name: "Suzuki GR 150 ",
+      price: "PKR 547,000",
+      category: "Bike",
+      caption: "Suzuki GS 150 is a popular 150cc bike in the Pakistani market. Suzuki GS 150 is said to be an accomplished bike for several reasons. One of the most prominent features this bike holds is the electric start system along with as well. An extremely energetic performance of 150cc 4 cooled SOHC engine is admired by the youth to a considerable extent." , 
+      color: ["Black", "Red"],
+      img: "https://cache3.pakwheels.com/system/bike_model_pictures/1408/original/1.jpg?1666268388",
+    },
+    
+
+  },
+
+  HONDA: {
+    CB150F: {
+      name: " CB 150F ",
+      price: "PKR:493,900",
+      caption: "One of the main reasons why many people choose to buy Honda cars is because of their reliability. Honda is known for making durable cars that last a long time, even with regular use, as long as they are properly maintained. That reliability is directly connected to Honda's dedication to production rules and guidelines",
+      category: "Bike",
+      color: ["Black", "Red","Silver"],    
+      img: "https://cache2.pakwheels.com/system/bike_model_pictures/1389/original/1.jpg?1665662024",
+    },
+    HondaCG125: {
+      name: "Honda CG 125 ",
+      price: "PKR 234,900",
+      caption: "One of the main reasons why many people choose to buy Honda cars is because of their reliability. Honda is known for making durable cars that last a long time, even with regular use, as long as they are properly maintained. That reliability is directly connected to Honda's dedication to production rules and guidelines",
+      category: "Bike",
+      color: ["Black", "Red","Silver"],    
+      img: "https://cache1.pakwheels.com/system/bike_model_pictures/1374/original/1.jpg?1665655628",
+    },
+    HondaCG125S : {
+      name: "Honda CG 125S  ",
+      price: "PKR 292,900",
+      caption: "One of the main reasons why many people choose to buy Honda cars is because of their reliability. Honda is known for making durable cars that last a long time, even with regular use, as long as they are properly maintained. That reliability is directly connected to Honda's dedication to production rules and guidelines",
+      category: "Bike",
+      color: ["Black"],    
+      img: "https://cache2.pakwheels.com/system/bike_model_pictures/2286/original/Side_Profile.png?1728904423",
+    },
+    HondaGoldWing: {
+      name: "Honda Gold Wing",
+      price: "PKR 49.0 lacs - 1.05 crore",
+      caption: "One of the main reasons why many people choose to buy Honda cars is because of their reliability. Honda is known for making durable cars that last a long time, even with regular use, as long as they are properly maintained. That reliability is directly connected to Honda's dedication to production rules and guidelines",
+      category: "Bike",
+      color: ["Black", "Red","Silver"],    
+      img: "https://cache3.pakwheels.com/system/bike_model_pictures/264/original/1.jpg?1397528997",
+    },
+    HondaCG125Dream: {
+      name: " Honda CG 125 Dream ",
+      price: "PKR 0.9 - 1.9 lacs",
+      caption: "One of the main reasons why many people choose to buy Honda cars is because of their reliability. Honda is known for making durable cars that last a long time, even with regular use, as long as they are properly maintained. That reliability is directly connected to Honda's dedication to production rules and guidelines",
+      category: "Bike",
+      color: ["Black", "Red","Silver"],    
+      img: "https://cache2.pakwheels.com/system/bike_model_pictures/932/original/Honda_CG_Dream_2015.jpg?1448536051",
+    },
+    HondaCBR150R : {
+      name: "Honda CBR 150R  ",
+      price: "PKR 0.7 - 2.5 lacs",
+      caption: "One of the main reasons why many people choose to buy Honda cars is because of their reliability. Honda is known for making durable cars that last a long time, even with regular use, as long as they are properly maintained. That reliability is directly connected to Honda's dedication to production rules and guidelines",
+      category: "Bike",
+      color: ["White"],    
+      img: "https://cache1.pakwheels.com/system/bike_model_pictures/927/original/2015_CBR.jpg?1448534061",
+    },
+  },
+  BMW: {
+    BMWS1000RR: {
+      name: "BMW S1000RR",
+      price: "PKR 12,600,000",
+      caption: "The term “pleasure” first appeared in the 1930s in BMW ads. It was later expanded to “Sheer Driving Pleasure.” The three-word claim was officially adopted in 1972, and, with a few exceptions, is used around the world to advertise BMW vehicles",
+      category: "Bike",
+      color: [ "White & Blue"],
+      img: "https://cache3.pakwheels.com/system/bike_model_pictures/2023/original/Bmw-new.jpg?1688727690",
+    },
+    BMWM1000R : {
+      name: "BMW M 1000 R ",
+      price: "PKR 0.7 - 13.0 lacs",
+      caption: "The term “pleasure” first appeared in the 1930s in BMW ads. It was later expanded to “Sheer Driving Pleasure.” The three-word claim was officially adopted in 1972, and, with a few exceptions, is used around the world to advertise BMW vehicles",
+      category: "Bike",
+      color: [ "Black & Red"],
+      img: "https://cache1.pakwheels.com/system/bike_model_pictures/2013/original/BMW-M-1000R.jpg?1684839101",
+    },
+    BMWR18B: {
+      name: "BMW R 18 B",
+      price: "PKR 97.0 lacs - 1.8 crore",
+      caption: "The term “pleasure” first appeared in the 1930s in BMW ads. It was later expanded to “Sheer Driving Pleasure.” The three-word claim was officially adopted in 1972, and, with a few exceptions, is used around the world to advertise BMW vehicles",
+      category: "Bike",
+      color: [ "Black"],
+      img: "https://cache1.pakwheels.com/system/bike_model_pictures/2242/original/BMW_R_18_Bagger.png?1725625739",
+    },
+    BMWR1250RS : {
+      name: "BMW R 1250 RS ",
+      price: "PKR 9,417,000",
+      caption: "The term “pleasure” first appeared in the 1930s in BMW ads. It was later expanded to “Sheer Driving Pleasure.” The three-word claim was officially adopted in 1972, and, with a few exceptions, is used around the world to advertise BMW vehicles",
+      category: "Bike",
+      color: [ "Blue"],
+      img: "https://cache2.pakwheels.com/system/bike_model_pictures/2012/original/BMW-RR1250-RS.jpg?1684838767",
+    },
+    BMWR1200GSAdventure : {
+      name: "BMW R1200 GS Adventure ",
+      price: "PKR 11,130,000",
+      caption: "The term “pleasure” first appeared in the 1930s in BMW ads. It was later expanded to “Sheer Driving Pleasure.” The three-word claim was officially adopted in 1972, and, with a few exceptions, is used around the world to advertise BMW vehicles",
+      category: "Bike",
+      color: [ "Red"],
+      img: "https://cache1.pakwheels.com/system/bike_model_pictures/1039/original/BMW_R1200_GS.jpg?1496140188",
+    },
+   
+  },
+};
+
+var selectedBrand;
+var selectedProduct;
+
+for(var brands in bikes){
+  brandSelect.innerHTML += `
+       
+                     <option value='${brands}' > ${brands} </option>
+         `;
+
+
+}
+
+function selectBrand(){
+     selectedBrand= event.target.value
+     productSelect.innerHTML=""
+    for(var products in bikes[selectedBrand]){
+        
+      productSelect.innerHTML += `
+             
+                   <option value='${bikes[selectedBrand][products].name}' > ${bikes[selectedBrand][products].name} </option>
+                   `;
+    }
+}
+function selectProduct(){
+  selectedProduct=event.target.value
+}
+
+// var brandSelect=document.getElementById('brandSelect');
+var productSelect=document.getElementById('productSelect');
+
+var mainCard = document.getElementById('mainCard');
+
+
+
+function searchProduct()  {
+  for (var keys in bikes[selectedBrand]) {
+        if (bikes[selectedBrand][keys].name === selectedProduct) {
+          // console.log(watches[selectedBrand][keys]);
+          var productImage = bikes[selectedBrand][keys].img;
+          // var productImageUrl =selectedProduct.img; // Get the image URL
+          var selectedCaption = bikes[selectedBrand][keys].caption;
+          var selectedPrice = bikes[selectedBrand][keys].price;
+          mainCard.innerHTML =`
+          <div class="row">
+          <div class="card mb-3 col-lg-6" style="max-width: 540px; max-height: 540px;">
+
+            <div class="">
+              <img  src="${productImage}" class="img-fluid rounded-start product-image"   alt="${selectedProduct}">
+            </div>
+          
+          </div>
+          <div class="col-lg-6"> 
+          <h3>${selectedProduct}</h3>
+          <div class="d-flex">
+          <p>5.0</p><i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+          <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+          <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+          <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+          <i class="fa-solid fa-star" style="color: #FFD43B;"></i>
+          <p>Rating</p></div>
+
+          <div class="d-flex  justify-content-between">
+          <div>
+          <p>Price</P>
+          <h3>${selectedPrice}</h3>
+          </div>
+          <div>
+          <p>Availability</P>
+          <h5>In Stock</h5>
+          </div>
+          </div>
+          <div>
+          <p>${selectedCaption}</p>
+          </div>
+          <div>
+          <button class="btn btn-primary">Buy Now</button>
+          <button class="btn btn-primary">Add to Cart</button>
+          </div>
+          </div>
+
+           `
+        }
+   
+   
+      document.getElementById('card-sec').style.display = 'none';
+      document.getElementById('mainCard').style.display = 'block';
+      document.getElementById('main-Image').style.display = 'none';
+      document.getElementById('Stylig').style.display = 'none';
+      }
+}
+                
+
+
+
+
+
+for (brands in bikes) {
+  for (keys in bikes[brands]) {
+    document.getElementById("card-sec").innerHTML += `
+          
+            <div class=" mt-5 col-12 col-md-4 ">
+              <div class="card  " style="width: 18rem; ">
+                <img  src="${bikes[brands][keys].img}"
+                  class="card-img-top" alt="...">
+                <div class="card-body text-center">
+                  <h4>${bikes[brands][keys].name}</h4>
+                </div>
+              </div>
+            </div>
+
+        
+            `;
+  }
+}
+
+
+
+
+
